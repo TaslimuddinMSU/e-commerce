64a1eb75027ced71ed119357c62a93b91968d8ef
TaslimuddinMSU <taslimuddin@msu.edu.in>
2025-02-14 18:29:58 +0530

    update

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,117 +2,36 @@
 import Image from "next/image";
 import React from "react";
 import { useRef, useState, useEffect } from 'react';
-import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+import {FaShoppingCart } from 'react-icons/fa';
 import AddToCart from "@/components/AddToCart"
 import { products } from "@/utils/Data";
 const Products = () => {
-    // const products = [
-    //     {
-    //         "id": 1,
-    //         "name": "Fresh Salmon",
-    //         "image": "/product-thumb-6.png",
-    //         "rating": 5,
-    //         "reviews": 222,
-    //         "originalPrice": 24.0,
-    //         "discountedPrice": 18.0,
-    //         "discount": "10% OFF"
-    //     },
-    //     {
-    //         "id": 2,
-    //         "name": "Imported Italian Spaghetti Pasta",
-    //         "image": "/product-thumb-6.png",
-    //         "rating": 5,
-    //         "reviews": 222,
-    //         "originalPrice": 24.0,
-    //         "discountedPrice": 18.0,
-    //         "discount": "10% OFF"
-    //     },
-    //     {
-    //         "id": 3,
-    //         "name": "Granny Smith Apples",
-    //         "image": "/product-thumb-6.png",
-    //         "rating": 5,
-    //         "reviews": 222,
-    //         "originalPrice": 24.0,
-    //         "discountedPrice": 18.0,
-    //         "discount": "10% OFF"
-    //     },
-    //     {
-    //         "id": 4,
-    //         "name": "Organic 2% Reduced Fat Milk",
-    //         "image": "/product-thumb-6.png",
-    //         "rating": 5,
-    //         "reviews": 222,
-    //         "originalPrice": 24.0,
-    //         "discountedPrice": 18.0,
-    //         "discount": "10% OFF"
-    //     },
-    //     {
-    //         "id": 5,
-    //         "name": "Greek Style Plain Yogurt",
-    //         "image": "/product-thumb-6.png",
-    //         "rating": 5,
-    //         "reviews": 222,
-    //         "originalPrice": 24.0,
-    //         "discountedPrice": 18.0,
-    //         "discount": "10% OFF"
-    //     },
-    //     {
-    //         "id": 5,
-    //         "name": "Greek Style Plain Yogurt",
-    //         "image": "/product-thumb-6.png",
-    //         "rating": 5,
-    //         "reviews": 222,
-    //         "originalPrice": 24.0,
-    //         "discountedPrice": 18.0,
-    //         "discount": "10% OFF"
-    //     }, 
-    //     {
-    //         "id": 5,
-    //         "name": "Greek Style Plain Yogurt",
-    //         "image": "/product-thumb-6.png",
-    //         "rating": 5,
-    //         "reviews": 222,
-    //         "originalPrice": 24.0,
-    //         "discountedPrice": 18.0,
-    //         "discount": "10% OFF"
-    //     }, 
-    //     {
-    //         "id": 5,
-    //         "name": "Greek Style Plain Yogurt",
-    //         "image": "/product-thumb-6.png",
-    //         "rating": 5,
-    //         "reviews": 222,
-    //         "originalPrice": 24.0,
-    //         "discountedPrice": 18.0,
-    //         "discount": "10% OFF"
-    //     },
-    //     {
-    //         "id": 5,
-    //         "name": "Greek Style Plain Yogurt",
-    //         "image": "/product-thumb-6.png",
-    //         "rating": 5,
-    //         "reviews": 222,
-    //         "originalPrice": 24.0,
-    //         "discountedPrice": 18.0,
-    //         "discount": "10% OFF"
-    //     }
-    // ]
 
     const scrollRef = useRef(null);
     const [isAtStart, setIsAtStart] = useState(true);
     const [isAtEnd, setIsAtEnd] = useState(false);
+    const [quantities, setQuantities] = useState({});
+    const [allItemsToCart, setAllItemsToCart] = useState([]);
 
+    const handleAddItems = (id) => {
+        const item = products.find((prod) => prod.id === id);
+        if (item) {
+            setAllItemsToCart((prevItems) => [...prevItems, item]);
+        }
+    }
+    console.log("handleAddItems", allItemsToCart)
+    const handleRemoveItems  = (id) => {
+        console.log("handleRemoveItems", id)
+    }
 
     return (
         <div className="w-full relative flex items-center justify-center">
             <div className="w-full flex gap-8 overflow-x-auto p-4 hide_scrollbar scroll-snap">
-                {products.map((product) => (
+                {products.map((product, index) => (
                     <div
                         key={product.id}
                         className="relative group border rounded-lg shadow-lg w-64 flex-shrink-0 scroll-snap-center cursor-pointer"
                     >
-                        {/* Product Image */}
                         <div className="flex items-center justify-center w-full h-auto p-4">
                             <Image
                                 src={product.image}
@@ -122,8 +41,6 @@ const Products = () => {
                                 height={100}
                             />
                         </div>
-
-                        {/* Product Details */}
                         <div className="p-4">
                             <h3 className="text-lg font-semibold truncate">{product.name}</h3>
                             <div className="flex items-center space-x-1 text-yellow-500">
@@ -133,15 +50,30 @@ const Products = () => {
                             <div className="flex items-baseline justify-between space-x-2">
                                 <div className="flex gap-3">
                                     <span className="text-gray-400 line-through">
-                                        ${product.originalPrice.toFixed(2)}
+                                        ₹{product.originalPrice.toFixed(2)}
                                     </span>
                                     <span className="text-lg font-bold text-green-600">
-                                        ${product.discountedPrice.toFixed(2)}
+                                        ₹{product.discountedPrice.toFixed(2)}
                                     </span>
                                 </div>
                                 <span className="text-sm font-medium text-red-500">
                                     ({product.discount})
                                 </span>
+                            </div>
+                            <div className="w-full flex gap-3 items-center justify-between pt-2">
+                                {/* <div className="w-[70px] flex items-center gap-2">
+                                    <button className="px-2 py-1 text-gray-600 border border-gray-300 rounded-md hover:bg-gray-200"
+                                        onClick={() => handleRemoveItems(product.id)}
+                                    >-</button>
+                                    <span className="text-sm text-slate-950">{allItemsToCart.length}</span>
+                                    <button onClick={ () => handleAddItems(product.id)} className="px-2 py-1 text-gray-600 border border-gray-300 rounded-md hover:bg-gray-200">+</button>
+                                </div> */}
+                                <button className="w-full flex items-center justify-center gap-2 bg-green-600 hover:bg-yellow-700 text-white px-4 py-2 rounded-md text-sm">
+                                    <span className="text-center"><FaShoppingCart /></span> <span className="text-sm text-white">Add to Cart</span>
+                                </button>
+                                <button className="w-full flex items-center justify-center gap-2 bg-yellow-500 text-white px-4 py-2 rounded-md text-sm">
+                                    <span className="text-sm text-white">View Detail</span>
+                                </button>
                             </div>
                         </div>
                     </div>
